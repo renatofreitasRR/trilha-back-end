4584c63ba45d8ab3eadbf4666cee3758ffbaf4e0
renatofreitasRR <natogfreitas@gmail.com>
2024-04-07 22:03:07 -0300

    add database-config

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,32 @@
 import fastify from 'fastify'
+import { knex } from './database';
+
 
 const app = fastify()
+
+const PORT = 3333;
+
+app.get('/', () => {
+    return 'Running'
+});
+
+app.get('/db', async () => {
+    const tables = await knex('sqlite_schema').select('*')
+
+    console.log("TABLES", tables);
+
+    return tables
+});
 
 app.get('/hello', () => {
     return 'Hello World'
 });
 
 app.listen({
-    port: 3333
+    port: PORT
 })
     .then(() => {
-        console.log("Running!")
+        console.log(`Running on port http://localhost:${PORT}`)
     })
     .catch((err) => {
         console.log("Error", err);
